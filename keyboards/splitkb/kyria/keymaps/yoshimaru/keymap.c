22f95c4250979a7e8ac975ce4ee2dd1847657d39
yoshimaru46 <yshr446@gmail.com>
2022-10-18 14:30:59 +0900

    WIP

--- keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
@@ -32,6 +32,7 @@ enum layers {
 #define FKEYS MO(_FUNCTION)
 
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
+#define CTL_TAB MT(MOD_LCTL, KC_TAB)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
 #define ALT_ENT MT(MOD_LALT, KC_ENT)
@@ -40,47 +41,51 @@ enum layers {
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
 // produces the key `tap` when tapped (i.e. pressed and released).
 
+// TODO
+// - Swap Tab & Esc
+// - Remove CTL_T(KC_SPC)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
+ * |  Esc   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
+ * |Ctrl/Tab|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  DEL   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |      |  |      |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |      |  |      |  ] } |   N  |   M  | ,  < | . >  | /  ? | ' "    |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | MUTE | LALT | LGUI | Sym  |Space |  |Shift/| FKEYS| RGUI | AltGr|      |
  *                        |      |      |      |      |      |  |Enter |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q , KC_W, KC_E , KC_R ,   KC_T   ,                                          KC_Y   , KC_U , KC_I   , KC_O  , KC_P   , KC_BSPC,
-     CTL_ESC , KC_A , KC_S, KC_D , KC_F ,   KC_G   ,                                          KC_H   , KC_J , KC_K   , KC_L  , KC_SCLN, CTL_QUOT,
-     KC_LSFT , KC_Z , KC_X, KC_C , KC_V ,   KC_B   , KC_LBRC , _______, _______   , KC_RBRC , KC_N   , KC_M , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                         KC_MUTE , KC_LALT, KC_LGUI, SYM     , KC_SPC , KC_SFTENT , FKEYS   , KC_RGUI, KC_RALT, _______
+     KC_ESC  , KC_Q , KC_W, KC_E , KC_R ,   KC_T   ,                                          KC_Y   , KC_U , KC_I   , KC_O  , KC_P   , KC_BSPC,
+     CTL_TAB , KC_A , KC_S, KC_D , KC_F ,   KC_G   ,                                          KC_H   , KC_J , KC_K   , KC_L  , KC_SCLN, KC_DEL ,
+     KC_LSFT , KC_Z , KC_X, KC_C , KC_V ,   KC_B   , KC_LBRC , _______       , _______   , KC_RBRC , KC_N   , KC_M , KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+                         KC_MUTE , KC_LALT, KC_LGUI, SYM     , CTL_T(KC_SPC) , KC_SFTENT , FKEYS   , KC_RGUI, KC_RALT, _______
     ),
 
 /*
  * Sym Layer: Numbers and symbols
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |    `   |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |   =    |
+ * |        |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |    ~   |  !   |  @   |  #   |  $   |  %   |                              |   ^  |  &   |  *   |  (   |  )   |   +    |
+ * |        |  !   |  @   |  #   |  $   |  %   |                              |   ^  |  &   |  *   |  (   |  )   |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |    |   |   \  |  :   |  ;   |  -   |  [   |  {   |      |  |      |   }  |   ]  |  _   |  ,   |  .   |  /   |   ?    |
+ * |        |   `  |  ~   |  \   |  |   |  [   |  {   |      |  |      |   }  |   ]  |  -   |  _   |  =   |  +   |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_SYM] = LAYOUT(
-     KC_GRV  , KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_EQL ,
-     KC_TILD , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-     KC_PIPE , KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
+     _______ , KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5   ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , _______,
+     _______ , KC_EXLM, KC_AT ,  KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+     _______ , KC_GRV,  KC_TILD, KC_BSLS, KC_PIPE, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -88,21 +93,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Function Layer: Function keys
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |  F9  | F10  | F11  | F12  |      |                              |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |  F5  |  F6  |  F7  |  F8  |      |                              |      | Left | Down |  UP  | Right|        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |                              |      | Left | Down |  UP  | Right|        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |  F1  |  F2  |  F3  |  F4  |      |      |      |  |      |      |      | Shift| Ctrl |  Alt |  GUI |        |
+ * |        |  F6  |  F7  |  F8  |  F9  |  F10 | F11  | F12  |  |      |      |      | Shift| Ctrl |  Alt |  GUI |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_FUNCTION] = LAYOUT(
-      _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     _______, _______, _______, _______, _______, _______,
-      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
-      _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+      _______,  _______,  _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
+      _______,  KC_F1  ,  KC_F2  ,  KC_F3 ,  KC_F4 , KC_F5  ,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
+      _______,  KC_F6  ,  KC_F7  ,  KC_F8 ,  KC_F9 , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______, _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
 
