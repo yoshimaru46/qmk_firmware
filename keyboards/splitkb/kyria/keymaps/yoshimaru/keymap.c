3bc338d8028894df09aa3b3f6de5bd36831cc790
yoshimaru46 <yshr446@gmail.com>
2022-10-19 23:22:37 +0900

    WIP

--- keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
@@ -45,6 +45,14 @@ enum layers {
 // - Swap Tab & Esc
 // - Remove CTL_T(KC_SPC)
 
+// Shift + ( = <
+const key_override_t left_paren_angle_bracket_override = ko_make_basic(MOD_MASK_SHIFT, KC_LEFT_PAREN, KC_LEFT_ANGLE_BRACKET);
+
+// Shift + ) = >
+const key_override_t right_paren_angle_bracket_override = ko_make_basic(MOD_MASK_SHIFT, KC_RIGHT_PAREN, KC_RIGHT_ANGLE_BRACKET);
+
+const key_override_t **key_overrides = (const key_override_t *[]){&left_paren_angle_bracket_override, &right_paren_angle_bracket_override, NULL};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -55,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                                      |------+------+------+------+------+--------|
  * |Ctrl/Tab|   A  |   S  |   D  |   F  |   G  |                                      |   H  |   J  |   K  |   L  | ;  : |  DEL   |
  * |--------+------+------+------+------+------+-------------.          ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |      |          |      |  ] } |   N  |   M  | ,  < | . >  | /  ? | ' "    |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  | ( <  |          | ) >  | ] }  |   N  |   M  | ,  < | . >  | /  ? | ' "    |
  * `----------------------+------+------+------+------+------|          |------+------+------+------+------+----------------------'
  *                        | MUTE | LALT | LGUI | Sym  |Space |          |Shift/| FKEYS| RGUI | AltGr|      |
  *                        |      |      |      |      |      |          |Enter |      |      |      |      |
@@ -64,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_ESC  , KC_Q , KC_W, KC_E , KC_R ,   KC_T   ,                                                 KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , KC_BSPC,
      CTL_TAB , KC_A , KC_S, KC_D , KC_F ,   KC_G   ,                                                 KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_DEL ,
-     KC_LSFT , KC_Z , KC_X, KC_C , KC_V ,   KC_B   , KC_LBRC , _______       , _______   , KC_RBRC , KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+     KC_LSFT , KC_Z , KC_X, KC_C , KC_V ,   KC_B   , KC_LBRC , KC_LPRN       , KC_RPRN   , KC_RBRC , KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
                          KC_MUTE , KC_LALT, KC_LGUI, SYM     , CTL_T(KC_SPC) , KC_SFTENT , FKEYS   , KC_RGUI, KC_RALT, _______
     ),
 
