8a6a5e22b691af58ca69711f835517008289ce5f
yoshimaru46 <yshr446@gmail.com>
2022-10-21 04:16:14 +0900

    WIP

--- keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                                      |------+------+------+------+------+--------|
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |                                      |      | Left | Down |  UP  | Right|        |
  * |--------+------+------+------+------+------+-------------.          ,-------------+------+------+------+------+------+--------|
- * |        |  F6  |  F7  |  F8  |  F9  |  F10 | F11  | F12  |          |      |      |      | Shift| Ctrl |  Alt |  GUI |        |
+ * |        |  F6  |  F7  |  F8  |  F9  |  F10 | F11  | F12  |          |      |      |      | Shift| VOLD | VOLU |  GUI |        |
  * `----------------------+------+------+------+------+------|          |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |          |      |      |      |      |      |
  *                        `----------------------------------'          `----------------------------------'
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FUNCTION] = LAYOUT(
       _______,  KC_1   ,  KC_2   ,  KC_3  ,  KC_4  , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
       _______,  KC_F1  ,  KC_F2  ,  KC_F3 ,  KC_F4 , KC_F5  ,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
-      _______,  KC_F6  ,  KC_F7  ,  KC_F8 ,  KC_F9 , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______, _______,
+      _______,  KC_F6  ,  KC_F7  ,  KC_F8 ,  KC_F9 , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_RSFT, KC_VOLD, KC_VOLU, KC_RGUI, _______, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
@@ -207,18 +207,18 @@ bool oled_task_user(void) {
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    } else if (index == 1) {
-        // Page up/Page down
+        // Next Tab/Previous Tab
         if (clockwise) {
             tap_code16(G(S(KC_RBRC)));
         } else {
             tap_code16(G(S(KC_LBRC)));
+        }
+    } else if (index == 1) {
+        // Page up/Page down
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
         }
     }
     return false;
