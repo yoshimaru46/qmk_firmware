59fa4e2c6c30abefbde9e83c1cf81843c484221f
yoshimaru46 <yshr446@gmail.com>
2022-10-21 20:48:58 +0900

    WIP

--- keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yoshimaru/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.          ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  | ( <  |          | ) >  | ] }  |   N  |   M  | ,  < | . >  | /  ? | ' "    |
  * `----------------------+------+------+------+------+------|          |------+------+------+------+------+----------------------'
- *                        | MUTE | LALT | LGUI | Sym  |Ctrl/ |          |Shift/| FKEYS|      |      |      |
+ *                        | MUTE | LALT | LGUI | Sym  |Ctrl/ |          |Shift/| FKEYS| RGUI |      |      |
  *                        |      |      |      |      |SPC   |          |Enter |      |      |      |      |
  *                        `----------------------------------'          `----------------------------------'
  */
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC  , KC_Q , KC_W, KC_E , KC_R ,   KC_T   ,                                                 KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , KC_DEL,
      KC_TAB  , KC_A , KC_S, KC_D , KC_F ,   KC_G   ,                                                 KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, _______,
      KC_LSFT , KC_Z , KC_X, KC_C , KC_V ,   KC_B   , KC_LBRC , KC_LPRN       , KC_RPRN   , KC_RBRC , KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
-                         KC_MUTE , KC_LALT, KC_LGUI, SYM     , CTL_SPC       , KC_SFTENT , FKEYS   , _______, _______, _______
+                         KC_MUTE , KC_LALT, KC_LGUI, SYM     , CTL_SPC       , KC_SFTENT , FKEYS   , KC_RGUI, _______, _______
     ),
 
 // NOTE
